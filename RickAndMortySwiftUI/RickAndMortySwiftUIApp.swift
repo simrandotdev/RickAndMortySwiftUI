c40e4664162105bd08f85e712cc72927f1582b6a
Simran Preet Singh <simran5590@gmail.com>
2022-12-27 22:50:35 +0530

    Initial Commit

---
+++ RickAndMortySwiftUI/RickAndMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySwiftUIApp.swift
+//  RickAndMortySwiftUI
+//
+//  Created by Simran Preet Narang on 2022-12-27.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
